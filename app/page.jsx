f772047cc6a5ea7c0bb59ae266bbb95006668c64
Guillermo Flores <gflores@crp.pe>
2023-05-17 22:23:51 -0500

    tabla, cambiar a react-select

--- app/page.jsx
+++ app/page.jsx
@@ -3,15 +3,15 @@ import Section_Home from "@/components/Section_Home";
 import Sidebar from "@/components/Sidebar";
 
 const Home = () => {
-    return (
-        <div className="w-full flex-center flex-col">
-            <Nav />
-            <div className=" w-full flex" >
-                <Sidebar />
-                <Section_Home />
-            </div>
-        </div>
-    );
+  return (
+    <div className="w-full flex-center flex-col">
+      <Nav />
+      <div className=" w-full flex">
+        <Sidebar />
+        <Section_Home />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
--- components/Section_Home.jsx
+++ components/Section_Home.jsx
@@ -1,47 +1,138 @@
+"use client";
+
 import "@styles/Section_home.css";
+import { metaDataGab, metaColums, dataAdvertisers } from "@util/datPrueba";
+import { useMemo, useState, useRef, forwardRef, useEffect } from "react";
+import { useRowSelect, useTable } from "react-table";
+import SelectBox from "./SelectBox";
 
-const Section_Home = () => {
+const IndeterminateCheckbox = forwardRef(({ indeterminate, ...rest }, ref) => {
+  const defaultRef = useRef();
+  const resolvedRef = ref || defaultRef;
+
+  useEffect(() => {
+    resolvedRef.current.indeterminate = indeterminate;
+  }, [resolvedRef, indeterminate]);
+
   return (
-    <div className='section_content relative flex flex-col w-full ' >
-      <section className="w-full relative wrap-table b-blank container_table">
-        <div className="table-first-column" >
-          <table className="tbl-1" >
-            <thead>
-              <tr>
-                <th>Employees</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td className="column1" >Brandon Green</td>
-              </tr>
-              <tr>
-                <td className="column1">Kathy Daniels</td>
-              </tr>
-              <tr>
-                <td className="column1">Elizabeth Alvarado</td>
-              </tr>
-              <tr>
-                <td className="column1">Michael Coleman</td>
-              </tr>
-              <tr>
-                <td className="column1">Jason Cox</td>
-              </tr>
-              <tr>
-                <td className="column1">Christian Perkins</td>
-              </tr>
-              <tr>
-                <td className="column1">Emily Wheeler</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-      </section>
-      <section>
-        Section 2
-      </section>
-    </div>
-  )
-}
+    <>
+      <input type="checkbox" ref={resolvedRef} {...rest} />
+    </>
+  );
+});
+const Section_Home = () => {
+  const data = useMemo(() => metaDataGab, []);
+  const columns = useMemo(() => metaColums, []);
+  const tableInstance = useTable({ columns, data }, useRowSelect, (hooks) => {
+    hooks.visibleColumns.push((columns) => [
+      {
+        id: "selection",
+        Header: ({ getToggleAllRowsSelectedProps }) => (
+          <div>
+            <IndeterminateCheckbox {...getToggleAllRowsSelectedProps()} />
+          </div>
+        ),
+        Cell: ({ row }) => (
+          <div>
+            <IndeterminateCheckbox {...row.getToggleRowSelectedProps()} />
+          </div>
+        ),
+      },
+      ...columns,
+    ]);
+  });
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+    state: { selectedRowIds },
+  } = tableInstance;
 
-export default Section_Home
+  const handleGetSelectedData = () => {
+    const selectedData = rows
+      .filter((row) => selectedRowIds[row.id])
+      .map((row) => row.original);
+    console.log(selectedData);
+  };
+
+  return (
+    <div className="section_content relative flex flex-col w-full ">
+      <div className="content-top-filter" >
+        <form className="relative w-full background-blank p-2 border-radius-5px flex justify-start items-center z-10">
+          <div className="relative flex justify-start items-center ">
+            <span className="text-label-filter">Advertiser: </span>
+            <div
+              className="relative"
+              style={{ height: "42px", width: "250px" }}
+            >
+              <SelectBox data={dataAdvertisers} />
+            </div>
+          </div>
+        </form>
+      </div>
+      <section className="w-full relative wrap-table b-blank container_table">
+        <table
+          className="table-main w-full"
+          {...getTableProps()}
+          style={{
+            minWidth: "100%",
+            borderCollapse: "collapse",
+            border: "1px solid black",
+          }}
+        >
+          <thead>
+            {headerGroups.map((headerGroups, i) => (
+              <tr key={i} {...headerGroups.getHeaderGroupProps()}>
+                {headerGroups.headers.map((column, j) => (
+                  <th
+                    key={j}
+                    {...column.getHeaderProps()}
+                    style={{
+                      position: "sticky",
+                      top: 0,
+                      zIndex: 1,
+                    }}
+                  >
+                    <div
+                      style={{
+                        background: "#fcf8ff",
+                        padding: "0.5rem",
+                        borderBottom: "1px inset #000",
+                      }}
+                    >
+                      {column.render("Header")}
+                    </div>
+                  </th>
+                ))}
+              </tr>
+            ))}
+          </thead>
+          <tbody {...getTableBodyProps()}>
+            {rows.map((row, i) => {
+              prepareRow(row);
+              return (
+                <tr key={i} {...row.getRowProps()}>
+                  {row.cells.map((cell, j) => {
+                    return (
+                      <td key={j} {...cell.getCellProps()}>
+                        {cell.render("Cell")}
+                      </td>
+                    );
+                  })}
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+        <button onClick={handleGetSelectedData}>
+          Obtener Datos seleccionados
+        </button>
+      </section>
+      <section>Section 2</section>
+    </div>
+  );
+};
+
+export default Section_Home;
